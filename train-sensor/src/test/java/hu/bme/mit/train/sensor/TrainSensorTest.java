7e98d494a4be8e42d7f1e0d120a1e5b8949a3404
bferi2001 <barnafeco@gmail.com>
2023-04-24 18:00:46 +0200

    importFixedAtTests6

--- train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
+++ train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
@@ -8,9 +8,7 @@ import hu.bme.mit.train.interfaces.TrainController;
 import hu.bme.mit.train.interfaces.TrainSensor;
 import hu.bme.mit.train.interfaces.TrainUser;
 import hu.bme.mit.train.sensor.TrainSensorImpl;
-import hu.bme.mit.train.controller.TrainControllerImpl; 
 
-import hu.bme.mit.train.user.TrainUserImpl;
 public class TrainSensorTest {
      
 	TrainController controller;
@@ -23,8 +21,8 @@ public class TrainSensorTest {
 	
 	@Before
 	public void before() {
-		controller = mock(TrainControllerImpl.class); 
-        user = mock(TrainUserImpl.class);
+		controller = mock(TrainController.class); 
+        user = mock(TrainUser.class);
         sensor = new TrainSensorImpl(controller, user); 
         
 		sensor.overrideSpeedLimit(50);
