7cc1ee83862b7405edc7be670e34d20635cb57be
bferi2001 <barnafeco@gmail.com>
2023-04-24 17:47:33 +0200

    importFixedAtTests4

--- train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
+++ train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
@@ -4,48 +4,50 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import static org.mockito.Mockito.*;
-import hu.bme.mit.train.controller.TrainControllerImpl;
+import hu.bme.mit.train.interfaces.TrainController;
+import hu.bme.mit.train.interfaces.TrainSensor;
+import hu.bme.mit.train.interfaces.TrainUser;
 import hu.bme.mit.train.sensor.TrainSensorImpl;
-import hu.bme.mit.train.user.TrainUserImpl;
-
 
 public class TrainSensorTest {
-    @Mock 
-	TrainControllerImpl controller;
+     
+	TrainController controller;
 	
 
-    TrainSensor sensor;
+    TrainSensorImpl sensor;
 	
-    @InjectMocks
-    TrainUserImpl user;
+    
+    TrainUser user;
 	
 	@Before
 	public void before() {
-		user = new TrainUserImpl(controller); 
-        sensor = new TrainSensorImpl(controller, user);
+		controller = mock(TrainControllerImpl.class); 
+        user = mock(TrainUserImpl.class);
+        sensor = new TrainSensorImpl(controller, user); 
+        
 		sensor.overrideSpeedLimit(50);
 	}
 
     @Test
     public void SpeedLimitSmallerThanZero() {
         sensor.overrideSpeedLimit(-2);
-        Assert.assertTrue(user.getAlarmState()); 
-    }
+        verify(user, times(1)).getAlarmState();
+        }
 
     @Test
     public void SpeedLimitBiggerThan500() {
         sensor.overrideSpeedLimit(505);
-        Assert.assertTrue(user.getAlarmState()); 
-    }
+        verify(user, times(1)).getAlarmState();
+        }
     @Test
     public void SpeedLimitBiggerThan500SmallerThanZero() {
         sensor.overrideSpeedLimit(250);
-        Assert.assertFalse(user.getAlarmState()); 
-    }
+        verify(user, times(1)).getAlarmState();
+        }
     @Test
     public void SpeedLimitSmallerThan50Percentage() {
-        controller.setSpeedLimit(400); 
+        when(controller.getReferenceSpeed()).thenReturn(400); 
         sensor.overrideSpeedLimit(150);
-        Assert.assertTrue(user.getAlarmState()); 
+        verify(user, times(1)).getAlarmState(); 
     }
 }
