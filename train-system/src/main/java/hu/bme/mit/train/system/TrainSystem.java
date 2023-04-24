c54e00473331477402065366fc09fe1eac6debe2
bferi2001 <barnafeco@gmail.com>
2023-04-24 15:41:08 +0200

    f1withThreads

--- train-system/src/main/java/hu/bme/mit/train/system/TrainSystem.java
+++ train-system/src/main/java/hu/bme/mit/train/system/TrainSystem.java
@@ -13,6 +13,10 @@ public class TrainSystem {
 	private TrainUser user = new TrainUserImpl(controller);
 	private TrainSensor sensor = new TrainSensorImpl(controller, user);
 
+	public TrainSystem(){
+		Thread th = new Thread(run());
+		
+	}	
 	public TrainController getController() {
 		return controller;
 	}
@@ -24,5 +28,8 @@ public class TrainSystem {
 	public TrainUser getUser() {
 		return user;
 	}
-
+	public void run(){
+		followSpeed();
+		sleep(100);
+	};
 }
